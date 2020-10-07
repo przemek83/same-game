c137b76d43ddc7a84197b99ed275846ff21cf8d6
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-10-07 14:29:17 +0200

    Test for makeMove function divided into 2.

--- test/SameGameTest.cpp
+++ test/SameGameTest.cpp
@@ -25,26 +25,6 @@ void performImpactGravityTest(std::vector<std::vector<int>> board,
         std::cout << "SUCCESS: impactGravity " + testName << std::endl;
 }
 
-void performMakeMoveTest(std::vector<std::vector<int>> board,
-                         bool (&checked)[MAX_W][MAX_H],
-                         int (&impactedColumns)[MAX_W], const Point& point,
-                         std::vector<std::vector<int>> expected,
-                         std::string testName)
-{
-    makeMove(board, checked, impactedColumns, point);
-    EXPECT_EQ(board, expected);
-    if (board != expected)
-    {
-        std::cout << "FAIL: makeMove " + testName << std::endl;
-        std::cout << "Got:" << std::endl;
-        printBoard(board);
-        std::cout << "Expected:" << std::endl;
-        printBoard(expected);
-    }
-    else
-        std::cout << "SUCCESS: makeMove " + testName << std::endl;
-}
-
 TEST(SameGameTest, impactGravity)
 {
     std::vector<std::vector<int>> board{{1, 3, 4, -1, 5}};
@@ -77,45 +57,67 @@ TEST(SameGameTest, impactGravity)
     performImpactGravityTest(board, impactedColumns, expected, "5");
 }
 
-TEST(SameGameTest, makeMove)
+TEST(SameGameTest, makeMove6x5)
 {
-    int impactedColumns[MAX_W];
-    std::vector<std::vector<int>> expected = {
-        {-1, -1, -1, -1, 1}, {-1, -1, -1, 1, 2},  {-1, -1, -1, 1, 1},
-        {-1, -1, -1, 1, 2},  {-1, -1, -1, -1, 1}, {-1, -1, 1, 1, 1}};
-    impactedColumns[0] = 4;
-    impactedColumns[1] = 4;
-    impactedColumns[2] = 4;
-    impactedColumns[3] = 3;
-    impactedColumns[4] = 4;
-    impactedColumns[5] = 3;
-    bool checked[MAX_W][MAX_H] = {};
+    const std::vector<std::vector<int>> expected{
+        {-1, -1, -1, -1, 1}, {-1, -1, -1, -1, 2}, {-1, -1, -1, -1, -1},
+        {-1, -1, -1, -1, 2}, {-1, -1, -1, -1, 1}, {-1, -1, 1, 1, 1}};
+    std::vector<std::vector<int>> board{{-1, -1, -1, -1, 1}, {-1, -1, -1, 1, 2},
+                                        {-1, -1, -1, 1, 1},  {-1, -1, -1, 1, 2},
+                                        {-1, -1, -1, -1, 1}, {-1, -1, 1, 1, 1}};
+    int impactedColumns[MAX_W]{};
+    std::memset(impactedColumns, EMPTY, sizeof(impactedColumns));
+
+    bool checked[MAX_W][MAX_H]{};
+    checked[1][3] = true;
     checked[2][3] = true;
     checked[2][4] = true;
     checked[3][3] = true;
-    std::memset(impactedColumns, EMPTY, sizeof(impactedColumns));
 
-    performMakeMoveTest(expected, checked, impactedColumns, {2, 3},
-                        {{-1, -1, -1, -1, 1},
-                         {-1, -1, -1, -1, 2},
-                         {-1, -1, -1, -1, -1},
-                         {-1, -1, -1, -1, 2},
-                         {-1, -1, -1, -1, 1},
-                         {-1, -1, 1, 1, 1}},
-                        "1");
+    makeMove(board, checked, impactedColumns, {2, 3});
+    EXPECT_EQ(board, expected);
 
-    if (checked[2][3] || checked[2][4] || checked[3][3])
-        std::cout << "FAIL: makeMove checked" << std::endl;
+    EXPECT_FALSE(checked[1][3]);
+    EXPECT_FALSE(checked[2][3]);
+    EXPECT_FALSE(checked[2][4]);
+    EXPECT_FALSE(checked[3][3]);
 
-    std::vector<std::vector<int>> board{
-        {0, 1, 0, 0}, {0, 1, 1, 1}, {1, 2, 2, 1}, {1, 2, 0, 2}};
-    expected = {{0, -1, 0, 0}, {0, -1, -1, -1}, {1, 2, 2, -1}, {1, 2, 0, 2}};
-    performMakeMoveTest(board, checked, impactedColumns, {2, 3}, expected, "2");
+    EXPECT_EQ(impactedColumns[0], EMPTY);
+    EXPECT_EQ(impactedColumns[1], 3);
+    EXPECT_EQ(impactedColumns[2], 4);
+    EXPECT_EQ(impactedColumns[3], 3);
+    EXPECT_EQ(impactedColumns[4], EMPTY);
 }
 
-static std::vector<std::vector<int>> symmetricalBoard{
-    {0, 1, 0, 0}, {0, 1, 1, 1}, {1, 2, 2, 1}, {1, 2, 0, 2}};
-static std::vector<std::vector<int>> singleRowBoard{{-1, 1, 0, 0}};
+TEST(SameGameTest, makeMove4x4)
+{
+    std::vector<std::vector<int>> board{
+        {0, 1, 0, 0}, {0, 1, 1, 1}, {1, 2, 2, 1}, {1, 2, 0, 2}};
+    const std::vector<std::vector<int>> expected{
+        {0, -1, 0, 0}, {0, -1, -1, -1}, {1, 2, 2, -1}, {1, 2, 0, 2}};
+    bool checked[MAX_W][MAX_H]{};
+    checked[0][1] = true;
+    checked[1][1] = true;
+    checked[1][2] = true;
+    checked[1][3] = true;
+    checked[2][3] = true;
+
+    int impactedColumns[MAX_W]{};
+    std::memset(impactedColumns, EMPTY, sizeof(impactedColumns));
+    makeMove(board, checked, impactedColumns, {2, 3});
+    EXPECT_EQ(board, expected);
+
+    EXPECT_FALSE(checked[0][1]);
+    EXPECT_FALSE(checked[1][1]);
+    EXPECT_FALSE(checked[1][2]);
+    EXPECT_FALSE(checked[1][3]);
+    EXPECT_FALSE(checked[2][3]);
+
+    EXPECT_EQ(impactedColumns[0], 1);
+    EXPECT_EQ(impactedColumns[1], 3);
+    EXPECT_EQ(impactedColumns[2], 3);
+    EXPECT_EQ(impactedColumns[3], EMPTY);
+}
 
 class GetClusterTests
     : public ::testing::TestWithParam<
@@ -128,12 +130,17 @@ TEST_P(GetClusterTests, GetCluster)
     const std::vector<std::vector<int>> board{std::get<0>(GetParam())};
     const int expectedClusterSize{std::get<1>(GetParam())};
     const Point point{std::get<2>(GetParam())};
+
     bool checked[MAX_W][MAX_H]{};
     std::memset(checked, false, sizeof(checked));
     unsigned int currentClusterSize{
         getClusterSize(board, point, checked, board.size(), board[0].size())};
     EXPECT_EQ(currentClusterSize, expectedClusterSize);
 }
+
+static std::vector<std::vector<int>> symmetricalBoard{
+    {0, 1, 0, 0}, {0, 1, 1, 1}, {1, 2, 2, 1}, {1, 2, 0, 2}};
+static std::vector<std::vector<int>> singleRowBoard{{-1, 1, 0, 0}};
 
 INSTANTIATE_TEST_SUITE_P(
     SameGameTest, GetClusterTests,
