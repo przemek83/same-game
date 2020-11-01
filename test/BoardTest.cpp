980370345ed956550fb177e0ee51b9ea33f2e0e8
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-11-01 21:10:59 +0100

    Added test for Board class getRowCount and getColumnCount methods.

--- test/BoardTest.cpp
+++ test/BoardTest.cpp
@@ -3,10 +3,26 @@
 #include "../Board.h"
 #include "TestTools.h"
 
-TEST(BoardTest, loadBoard)
+TEST(BoardTest, createBoard)
 {
     Board currentBoard{TestTools::prepareBoard(4, 4, "4x4_3_colors.txt")};
     std::vector<std::vector<int>> expectedBoard{
         {0, 1, 0, 0}, {0, 1, 1, 1}, {1, 2, 2, 1}, {1, 2, 0, 2}};
     EXPECT_EQ(currentBoard, TestTools::createBoard(expectedBoard));
+}
+
+TEST(BoardTest, getRowCount)
+{
+    const int expectedRowCount{2};
+    std::istringstream stringStream;
+    Board board(5, expectedRowCount, stringStream);
+    EXPECT_EQ(board.getRowCount(), expectedRowCount);
+}
+
+TEST(BoardTest, getColumnCount)
+{
+    const int expectedColumnCount{5};
+    std::istringstream stringStream;
+    Board board(expectedColumnCount, 2, stringStream);
+    EXPECT_EQ(board.getColumnCount(), expectedColumnCount);
 }
